67feb5164beab841ad06c3efe14ac3e49b68dde0
7Pawns <124451776+7Pawns@users.noreply.github.com>
2023-06-13 17:57:29 +0300

    completed anti-csrf tokens

--- CSRF/app.py
+++ CSRF/app.py
@@ -1,13 +1,21 @@
-from flask import Flask, render_template, url_for, request, make_response
+from flask import Flask, render_template, url_for, request, make_response, abort, session
 from flask_api import status
+from uuid import uuid4
 
 app = Flask(__name__)
+app.secret_key = uuid4().hex
 
+# Inital Pizzas Prices
+prices = {'p1' : '100$', 'p2' : '150$', 'p3' : '200$'}
+
+# Main page
 @app.route("/", methods=['GET', 'POST'])
 def index():
+    session['anti_csrf_token'] = uuid4().hex
+    print(session)
+    return render_template('index.html', prices=prices, anticsrf=session['anti_csrf_token'])
 
-    return render_template('index.html')
-
+# Simple cookie setter (just name)
 @app.route("/setcookie", methods=['POST'])
 def setcookie():
     if request.method == 'POST':
@@ -16,12 +24,19 @@ def setcookie():
         resp.set_cookie('name', content)
         return resp
 
+# API for changing prices that will be attacked using CSRF
 @app.route("/api", methods=['POST'])
 def changePrice():
+    if 'anti_csrf_token' not in request.form:
+        return abort(400)
     if request.cookies.get('name') != 'admin':
-        return '403 Forbidden', status.HTTP_403_FORBIDDEN
-    
-    return render_template('index.html', id=request.form['id'], price=request.form['price'])
+        return abort(403)
+    if session['anti_csrf_token'] != request.form['anti_csrf_token']:
+        return abort(401)
+    id = request.form['id']
+    price = request.form['price']
+    prices[id] = price + '$'
+    return render_template('index.html', prices=prices, anticsrf=session['anti_csrf_token'])
 
 if __name__ == "__main__":
     app.run(debug=True)
