b0fc53d2c731bfc0f627b67f70cebf87ea23e53f
7Pawns <124451776+7Pawns@users.noreply.github.com>
2023-06-13 14:55:10 +0300

    completed XSS

--- XSS/app.py
+++ XSS/app.py
@@ -1,9 +1,15 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods=['GET', 'POST'])
 def index():
+    if request.method == 'POST':
+        content = request.form['content']
+        if ('p1' in content or 'p2' in content or 'p3' in content):
+            return content + ' Adopted!'
+        else:
+            return 'Not a valid snake'
     return render_template('index.html')
 
 if __name__ == "__main__":
